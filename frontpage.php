bc10c5961c6c70b9896d24c773f20cbe7dd44752
Michael Kirk <michael.john.kirk@gmail.com>
2012-05-18 21:18:29 -0700

    day 1 of implementing hyperion theme

--- frontpage.php
+++ frontpage.php
@@ -32,14 +32,32 @@ Template Name: Hyperion Yard Front Page
 		</div>
 
 		<div id="upper-content">
-
-			<div id="project-photo">
-				photos!
-			</div>
-			<div id="project-text">
-				text ^_^
-			</div>
-		
+			<div id="project-box">							
+				<a href="/2011/11/22/a-place-to-grow-herbs/">
+					<div class="project-box-image">						
+						<img src="/wp-content/themes/hyperion/images/herbspiral.png"> 
+						<span>Garden Designs</span>
+					</div>
+				</a>
+				
+				<a href="/2012/03/12/worms-part-1/">
+					<div class="project-box-image">
+						<img src="/wp-content/themes/hyperion/images/composticon.png"> 
+						<span>Composting</span>				
+					</div>
+				</a>
+				
+				<a href="/2011/10/13/greywater-laundry-to-landscape/">
+					<div class="project-box-image">
+						<img src="/wp-content/themes/hyperion/images/greywatericon.png">
+						<span>Greywater</span>			
+					</div> 
+				</a>
+					<div id="project-box-text">	
+			The Hyperion yard is a collection of projects. My name is Linnea and I live at Hyperion house along with two housemates. We are building off the work of friends who lived here before us, who first tried to wrangle the backyard into being a food producing entity. The bermuda grass is ever creeping, and I’d say we’ve had more failures than successes at this point, but my hope is to document what we are doing and learn from it in the best way we can. Welcome to Hyperion Yard.	
+					</div>				
+							
+			</div>		
 		</div>
 			
 	</div>
