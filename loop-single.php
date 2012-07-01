5b4d07fe0c239463cffc9c5a3178d4779c92a9f8
Michael Kirk <michael.john.kirk@gmail.com>
2012-06-30 17:36:21 -0700

    refactor loop-single to use posted_on method like the main loop

--- loop-single.php
+++ loop-single.php
@@ -4,16 +4,25 @@ Template Name: Hyperion Yard Page
 */
 ?>
 
- 		
+
+<?php while ( have_posts() ) : the_post(); ?>
+
 	<div id="lower-content">
 		<div class="content-column">	
+			<h2 id="post-<?php the_ID(); ?>">
+				<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
+					<?php the_title(); ?>
+				</a>
+			</h2>
+					
+			<span class="time-author">
+				<?php hyperion_posted_on(); ?>
+			</span>		
 
-<h2 id="post-<?php the_ID(); ?>">
-<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
-<?php the_title(); ?></a></h2>
-<span class="time-author"><?php the_time('F jS, Y') ?> by <?php the_author() ?></span>		
-		<?php echo apply_filters('the_content', $post->post_content); ?>
+			<?php the_content() ?>
 		</div>
 	</div>
+
+<?php endwhile; ?>
 
 
